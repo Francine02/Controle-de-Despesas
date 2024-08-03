c28d34b5c5ad8a1739bb8a4d6f9f49f319c99c14
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-08-03 15:48:18 -0300

    Adicionado Cors e modificado aplication para o banco de dados funcionar

---
+++ backend/src/main/java/com/backend/cors/CorsConfig.java
@@ -0,0 +1,15 @@
+package com.backend.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("https://controle-de-despesas-production.up.railway.app")
+                .allowedMethods("GET", "POST", "DELETE", "PUT");
+    }
+}
