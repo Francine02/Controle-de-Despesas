d64630f08cd0002af2fa68c739ca13eefff79c7c
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-08-17 18:12:38 -0300

    Adicionado conexão com a api para registrar usuário

--- frontend/src/pages/CreateAcount.jsx
+++ frontend/src/pages/CreateAcount.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { BeginImg } from "../components/BeginImg";
 import { ButtonForm } from "../components/ButtonForm";
 import { Form } from "../components/Form";
@@ -5,6 +6,33 @@ import { useNavigate } from 'react-router-dom';
 
 export function CreateAcount() {
     const navigate = useNavigate();
+    const [newName, setNewName] = useState('')
+    const [newEmail, setNewEmail] = useState('')
+    const [newPassword, setNewPassword] = useState('')
+
+    const handleRegister = async (e) => {
+        e.preventDefault()
+        try{
+            const response = await fetch('https://controle-de-despesas-production.up.railway.app/auth/register', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({userName: newName, email: newEmail, password: newPassword})
+            })
+
+            if (response.ok) {
+                console.log("sucesso")
+                alert("Usuário registrado com sucesso!")
+                setTimeout(() => {
+                    navigate('/auth/login');
+                }, 6000); // Atraso de 6 segundos (6000 milissegundos)
+            } else {
+                alert("Preencha todos os campos!!")
+            }
+        }
+        catch(error) {
+            console.log('Erro ao registrar usuário: ', error)
+        }
+    }
 
     return (
         <div>
@@ -13,12 +41,27 @@ export function CreateAcount() {
                 <div className="py-12 lg:py-5 text-center lg:mx-32 lg:my-6">
                     <h1 className="text-3xl sm:text-4xl font-black tracking-wide mb-12 sm:mb-16">Cadastrar</h1>
                     <form className="inline-grid space-y-7 sm:space-y-8 w-full">
-                        <Form img="https://img.icons8.com/?size=100&id=ABBSjQJK83zf&format=png&color=000000" type="text" placeholder="Nome" />
+                        <Form
+                            onChange={(e) => setNewName(e.target.value)}
+                            value={newName}
+                            img="https://img.icons8.com/?size=100&id=ABBSjQJK83zf&format=png&color=000000"
+                            type="text"
+                            placeholder="Nome" />
 
-                        <Form img="https://img.icons8.com/?size=100&id=60688&format=png&color=000000" type="email" placeholder="E-mail" />
+                        <Form
+                            onChange={(e) => setNewEmail(e.target.value)}
+                            value={newEmail}
+                            img="https://img.icons8.com/?size=100&id=60688&format=png&color=000000"
+                            type="email"
+                            placeholder="E-mail" />
 
-                        <Form img="https://img.icons8.com/?size=100&id=15454&format=png&color=000000" type="password" placeholder="Senha" />
-                        <ButtonForm color="bg-[#4c0192]" text="Criar" />
+                        <Form 
+                            onChange={(e) => setNewPassword(e.target.value)}
+                            value={newPassword}
+                            img="https://img.icons8.com/?size=100&id=15454&format=png&color=000000" 
+                            type="password" 
+                            placeholder="Senha" />
+                        <ButtonForm onclick={handleRegister} color="bg-[#4c0192]" text="Criar" />
                         <p className="text-sm sm:text-base font-bold">Já possui uma conta? <a onClick={() => navigate('/auth/login')} className="text-[#1ecebc] font-black hover:opacity-70 cursor-pointer">Entrar</a></p>
                     </form>
                 </div>
