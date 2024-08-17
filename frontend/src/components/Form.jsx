8bd66b0d2cf9a24ccc77cb2303cbe26948a84193
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-08-17 13:29:04 -0300

    Adicionado icones dentro do input

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,11 +1,12 @@
-export function Form({ type, placeholder }) {
+export function Form({ img, type, placeholder }) {
     return (
-        <div className="w-full">
+        <div className="w-full relative">
             <input
                 type={type}
                 placeholder={placeholder}
-                className="w-full shadow-[0_3px_10px_rgb(0,0,0,0.2)] placeholder:pl-2 bg-[#f5f5f59c] py-2 px-4 sm:py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+                className="w-full shadow-[0_3px_10px_rgb(0,0,0,0.2)] placeholder:pl-2 bg-[#f5f5f59c] py-2 px-4 sm:py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-black pl-10 border"
             />
+            <img src={img} class="absolute top-1/2 left-3 w-5 h-5 transform -translate-y-1/2 opacity-80"></img>
         </div>
     )
 }
